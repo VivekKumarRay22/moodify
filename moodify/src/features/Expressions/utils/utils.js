e89baa5fd99b09daf6421bf3e84fc5036173c3d0
VivekKumarRay22 <vivekkumarray855@gmail.com>
2026-08-10 23:45:10 +0530

    removed setLoading and calling loadModel & startCamera

--- moodify/src/features/Expressions/utils/utils.js
+++ moodify/src/features/Expressions/utils/utils.js
@@ -1,46 +1,36 @@
-import { FilesetResolver, FaceLandmarker } from "@mediapipe/tasks-vision"
+import {FilesetResolver,FaceLandmarker,} from "@mediapipe/tasks-vision";
 
-// Load MediaPipe model
+export async function loadModel({
+  faceLandmarkerRef,
+}) {
+  const vision = await FilesetResolver.forVisionTasks(
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm"
+  );
 
-  export async function loadModel({ animationFrameRef, faceLandmarkerRef, videoRef }) {
-    const vision = await FilesetResolver.forVisionTasks(
-      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm",
-    )
-
-    const faceLandmarker = await FaceLandmarker.createFromOptions(vision, {
+  const faceLandmarker =
+    await FaceLandmarker.createFromOptions(vision, {
       baseOptions: {
         modelAssetPath:
           "https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/latest/face_landmarker.task",
       },
 
       runningMode: "VIDEO",
-
       numFaces: 1,
-
       outputFaceBlendshapes: true,
-
       outputFacialTransformationMatrixes: true,
-    })
+    });
 
-    faceLandmarkerRef.current = faceLandmarker
+  faceLandmarkerRef.current = faceLandmarker;
+}
 
-    setLoading(false)
+export async function startCamera(videoRef) {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+  });
+
+  if (videoRef.current) {
+    videoRef.current.srcObject = stream;
   }
 
-  loadModel()
-
-
-// Start webcam
-
-  export async function startCamera() {
-    const stream = await navigator.mediaDevices.getUserMedia({
-      video: true,
-    })
-
-    if (videoRef.current) {
-      videoRef.current.srcObject = stream
-    }
-  }
-
-  startCamera()
-
+  return stream;
+}
